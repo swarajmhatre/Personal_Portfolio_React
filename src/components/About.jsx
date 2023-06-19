6dec276e39e2b5516cc622343cd4fa7a730e51d5
swarajmhatre <swarajmhatre25@gmail.com>
2023-06-19 17:32:27 +0530

    Initial Push with modified content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,31 +2,31 @@ import React from "react";
 
 const About = () => {
   return (
-    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
-      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full" >
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-purple-500">About</p>
+          <p className="text-4xl font-bold inline border-b-4 border-purple-500">
+            About
+          </p>
         </div>
         <p className="text-xl mt-20">
-          Here we are on the third article of the Tailwind CSS series. I hope
-          you are enjoying it. This time around though, we will take the entire
-          process a notch up by caring more about different screen sizes, doing
-          a bit of clean-up of our HTML code with the help of some utilities of
-          our own, and adding common interactions to the hero section of the
-          webpage. As always there will be plenty of explanations and code
-          examples along the way. The best part is we will be using the demo
-          used in the previous article to make all things happen.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eos,
+          obcaecati nobis sint laboriosam quis porro est pariatur libero autem
+          facilis eligendi expedita accusantium. Mollitia totam eligendi illum
+          obcaecati molestias fugiat saepe non, sed aut magnam nisi assumenda
+          numquam, ullam ad laudantium. Unde, eligendi asperiores?
         </p>
         <br />
-        <p  className="text-xl">
-          Here we are on the third article of the Tailwind CSS series. I hope
-          you are enjoying it. This time around though, we will take the entire
-          process a notch up by caring more about different screen sizes, doing
-          a bit of clean-up of our HTML code with the help of some utilities of
-          our own, and adding common interactions to the hero section of the
-          webpage. As always there will be plenty of explanations and code
-          examples along the way. The best part is we will be using the demo
-          used in the previous article to make all things happen.
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+          doloremque nulla, sapiente consequatur voluptates adipisci repellendus
+          sequi aspernatur? Quas suscipit necessitatibus nisi harum obcaecati
+          fugiat odit molestias dolor, ipsam nostrum architecto numquam eveniet
+          sed ipsum maxime ullam! Voluptatum sunt dolore quisquam doloremque
+          deserunt labore dignissimos omnis officia! Voluptatum, at temporibus!
         </p>
       </div>
     </div>
